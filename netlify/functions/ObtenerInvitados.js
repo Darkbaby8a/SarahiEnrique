225ad48cb5b64e60be970d68864defa3bf5036aa
Daniel Ochoa <daniel.ochoacova@gmail.com>
2026-08-11 21:03:02 -0600

    cambios

--- netlify/functions/ObtenerInvitados.js
+++ netlify/functions/ObtenerInvitados.js
@@ -33,7 +33,7 @@ export const handler = async (event, context) => {
   try {
     // Consultar todos los invitados ordenados por los más recientes
     const query = `
-      SELECT id, "familiaidentificador", "familiades", "pases"
+      SELECT id, "familiaidentificador", "familiades", "pases","acepto","rechazo"
       FROM "sarahienrique" 
       ORDER BY id DESC;
     `;
